1ff7555db815c7b962b33fd9c3dbfaecfae2f176
mrkaiser <kaiserollofdoom@gmail.com>
2014-02-13 23:07:48 -0500

    Added new endpoints for tracks and locations

--- server.py
+++ server.py
@@ -10,7 +10,7 @@ import os
 @app.route('/people/', methods=['GET'])
 def people():
     if request.method == 'GET':
-        results = Person.query.limit(10).all()
+        results = Person.query.all()
 
         json_results = []
         for result in results:
@@ -23,6 +23,30 @@ def people():
 def events():
     if request.method == 'GET':
         results = Event.query.all()
+
+        json_results = []
+        for result in results:
+            json_results.append(result.to_json())
+
+        return jsonify(items=json_results)
+
+
+@app.route('/tracks/', methods=['GET'])
+def tracks():
+    if request.method == 'GET':
+        results = Track.query.all()
+
+        json_results = []
+        for result in results:
+            json_results.append(result.to_json())
+
+        return jsonify(items=json_results)
+
+
+@app.route('/locations/', methods=['GET'])
+def locations():
+    if request.method == 'GET':
+        results = Location.query.all()
 
         json_results = []
         for result in results:

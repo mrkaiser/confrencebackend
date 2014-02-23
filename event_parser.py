c720fa77875cfb1b09acd813063c9fb659be60ad
mrkaiser <kaiserollofdoom@gmail.com>
2014-02-23 10:18:29 -0500

    Added some tests for the event body scrapper. The tests will pass. Returns the body part where the important metadata is

--- event_parser.py
+++ event_parser.py
@@ -1,5 +1,13 @@
 __author__ = 'mrkaiser'
+import re
 
 
 def parser(event_body):
     True
+
+
+def strip_raw_event(event_body):
+    regex = r'.+;.+[\.!?]'
+    reversed = event_body[::-1]
+    rereverse = re.findall(regex, reversed)[0]
+    return rereverse[::-1]

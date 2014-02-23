8e062de64ac7573d4ef1aa5e82000dbcaf7775a2
mrkaiser <kaiserollofdoom@gmail.com>
2014-02-23 10:18:00 -0500

    Added some tests for the event body scrapper. The tests will pass. Returns the body part where the important metadata is

--- tests/event_body_scrapping.py
+++ tests/event_body_scrapping.py
@@ -1,6 +1,6 @@
 __author__ = 'mrkaiser'
 
-from event_parser import parser
+from event_parser import parser, strip_raw_event
 
 
 def test_event_factory_one():
@@ -8,3 +8,24 @@ def test_event_factory_one():
            'resident engineering geniuses. D. Taylor, R. Jones, C. Taylor; Fri 11:30 am; Crystal Ballroom [Hil]'
 
     assert parser(text) == True
+
+
+def test_strip_one():
+    text = 'The actors from some of your favorite cartoons give you insight on the people behind the voices!' \
+            ' B. West, K. Najimy, R. Horvitz, R. Simons, R. Paulsen, B. Carter; Sat 2:30 pm; Cent. II–III [HYT]'
+
+    assert strip_raw_event(text) == '! B. West, K. Najimy, R. Horvitz, R. Simons, R. Paulsen, B. Carter; ' \
+                                    'Sat 2:30 pm; Cent. II–III [HYT]'
+
+
+def test_strip_two():
+    text = 'A look at Frylock, Shake and Meatwad! Sat 4:00 pm; Dunwoody [HYT]'
+
+    assert strip_raw_event(text) == '! Sat 4:00 pm; Dunwoody [HYT]'
+
+
+def test_strip_three():
+    text = 'Fan group [adult swim] Central does a live panel and podcast about the black sheep of the Turner family.' \
+           ' Fri 5:30 pm; Dunwoody [HYT]'
+
+    assert strip_raw_event(text) == '. Fri 5:30 pm; Dunwoody [HYT]'
